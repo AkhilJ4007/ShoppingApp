81daca1ec6fbf357e1d7031a50c6960462cdd6d8
AkhilJ4007 <akhiljamesjoseph944@gmail.com>
2020-10-20 00:53:41 -0400

    Login Page UI frame done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage.component'
+
 
 function App() {
   return (
-  <div>
-    
-  </div>
+    <div>
+    <Homepage/>
+    </div>
   );
 }
 
