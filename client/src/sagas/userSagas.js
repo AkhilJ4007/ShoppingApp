9766684833d19d818abe5f6597393623d023302b
AkhilJ4007 <akhiljamesjoseph944@gmail.com>
2021-07-20 14:41:48 -0400

    Solving tokenerror

--- client/src/sagas/userSagas.js
+++ client/src/sagas/userSagas.js
@@ -25,7 +25,7 @@ function* login(action) {
       yield put({ type: alertTypes.alert, payload: user.message })
   }
   else {
-    console.log("Sagaa Tokennnn".user.token)
+    console.log("Sagaa Tokennnn".user)
     yield put({ type: userTypes.tokenSet, payload: user.token })
     yield put({ type: userTypes.getUser, payload: user.user })
   }
