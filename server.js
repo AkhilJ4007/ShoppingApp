6f97dabcf01ee7d2e5d79604d33cb97cd0c02ee7
AkhilJ4007 <akhiljamesjoseph944@gmail.com>
2021-08-03 15:43:04 -0400

    changed Bearer to bearer

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const { connect } = require('mongodb');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', 'https://akj-shopping.herokuapp.com')
 
     // const allowedOrigins = [ 'https://akj-shopping.herokuapp.com','http://localhost:3000'];
     // const origin = req.headers.origin;
